d5c073917a4e5fd49d571dab7d24852034401410
a7iiid <naz131681@gmail.com>
2024-07-24 16:20:15 +0300

    init project

---
+++ lib/core/utils/app_style.dart
@@ -0,0 +1,10 @@
+class AppStyle{
+  static TextStyle Bolde40= TextStyle(
+color: Color(0xFF001815),
+fontSize: 40,
+fontStyle: FontStyle.italic,
+fontFamily: 'Circular Std',
+fontWeight: FontWeight.w500,
+height: 0.03,
+);
+}
