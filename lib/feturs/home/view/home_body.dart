2bb77718263345f87b427c53d1dd3f0ec87999fe
a7iiid <naz131681@gmail.com>
2024-07-25 00:54:43 +0300

    add status and containr

--- lib/feturs/home/view/home_body.dart
+++ lib/feturs/home/view/home_body.dart
@@ -1,11 +1,9 @@
-import 'dart:ffi';
-
 import 'package:chat_app/core/assets.dart';
 import 'package:chat_app/core/utils/app_style.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import '../widget/hedar_home.dart';
+import '../widget/status.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
@@ -21,10 +19,32 @@ class HomeBody extends StatelessWidget {
           color: Colors.black,
         ),
         Positioned(
-          width: MediaQuery.sizeOf(context).width,
+          width: MediaQuery.of(context).size.width,
           top: 50,
           child: const HedarHome(),
-        )
+        ),
+        Positioned(
+          width: MediaQuery.of(context).size.width,
+          top: 130,
+          bottom: 0,
+          child: const Status(),
+        ),
+        Positioned(
+            top: MediaQuery.of(context).size.height * .3,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+              ),
+            ))
       ],
     );
   }
---
+++ lib/feturs/home/widget/status.dart
@@ -0,0 +1,37 @@
+import 'package:chat_app/core/utils/app_style.dart';
+import 'package:flutter/material.dart';
+
+class Status extends StatelessWidget {
+  const Status({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height - 130, // Constrain height
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 15,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  radius: 29,
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  "Ahmad",
+                  style: AppStyle.regular14,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
